d7492b83eae73c9c6e07b7a0baaf76cf526c8461
Romwil <romwil.pilapil@gmail.com>
2022-01-25 03:37:03 +0800

    database connection and query code

---
+++ backend.py
@@ -0,0 +1,56 @@
+import sqlite3
+
+def connect():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book (book_id INTEGER PRIMARY KEY, title TEXT,author TEXT, year INTEGER, isbn INTEGER)")
+    conn.commit()
+    conn.close()
+    
+def insert(t,a,y,i):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO book (title,author,year,isbn) VALUES (?,?,?,?)",(t,a,y,i))
+    conn.commit()
+    conn.close()
+    
+def view():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows = cur.fetchall()
+    conn.close()
+    
+    return rows
+
+def search(t="",a="",y="",i=""):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",(t,a,y,i))
+    rows = cur.fetchall()
+    conn.close()
+    
+    return rows
+
+def update(bi,t,a,y,i):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE book_id=?",(t,a,y,i,bi))
+    conn.commit()
+    conn.close()
+    
+def delete(i):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM book WHERE book_id=?",(i,))
+    conn.commit()
+    conn.close()
+
+connect()
+
+# insert("Books delete id 2","romwil",2022,9879564987)
+# print(view())
+# delete(2)
+update(1,"update test function","pilapil",2022,6543564654)
+
+print(view())
--- frontend.py
+++ frontend.py
@@ -16,6 +16,7 @@ close
 """
 
 from tkinter import *
+import backend
 
 window = Tk()
 
@@ -48,6 +49,31 @@ iEntry = Entry(window,textvariable=isbnEntry)
 iEntry.grid(row=1,column=3)
 
 listEntries = Listbox(window,height=6,width=35)
-listEntries.grid(row=2,)
+listEntries.grid(row=2,rowspan=6, column=0,columnspan=2)
+
+listScrollBar = Scrollbar(window)
+listScrollBar.grid(row=2,rowspan=6,column=2)
+
+listEntries.configure(yscrollcommand=listScrollBar.set)
+listScrollBar.configure(command=listEntries.yview)
+
+viewButton = Button(window,text="View all books",width=12)
+viewButton.grid(row=2,column=3)
+
+searchButton = Button(window,text="Search book",width=12)
+searchButton.grid(row=3,column=3)
+
+addButton = Button(window,text="Add book",width=12)
+addButton.grid(row=4,column=3)
+
+updateButton = Button(window,text="Update book",width=12)
+updateButton.grid(row=5,column=3)
+
+deleteButton = Button(window,text="Delete book",width=12)
+deleteButton.grid(row=6,column=3)
+
+closeButton = Button(window,text="Close",width=12)
+closeButton.grid(row=7,column=3)
+
 
 window.mainloop()
